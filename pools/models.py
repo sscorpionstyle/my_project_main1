cc3b6bab68517217d5cb3f9389dd1887c03328c3
sscorpionstyle <sscorpionstyle@mail.ru>
2023-06-04 14:47:20 +0300

    init

--- pools/models.py
+++ pools/models.py
@@ -4,5 +4,5 @@ from django.db import models
 # Create your models here.
 
 class Feedback(models.Model):
-    email = models.CharField(max_length=30)
+    email = models.CharField(max_length=35)
     text = models.TextField()
